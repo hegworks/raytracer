ce5373bd90f56073363dce1f494e15dbbbeb9446
Hesam Ghadimi <hess.gha@gmail.com>
2025-02-19 10:54:05 +0100

    fix(DBG.h) : make IsCloseF more precise & fix printing text out of screen

--- src/DBG.h
+++ src/DBG.h
@@ -5,10 +5,10 @@ inline bool tddResetCam = false;
 
 static bool DBGCanPrint(const float2 pos)
 {
-	return !(pos.x < 0 || pos.x >= SCRWIDTH || pos.y < 0 || pos.y >= SCRHEIGHT);
+	return !(pos.x < 0 || pos.x >= SCRWIDTH || pos.y < 0 || pos.y >= SCRHEIGHT - 10);
 }
 
 static bool IsCloseF(const float a, const float b)
 {
-	return abs(a - b) < 0.01;
+	return abs(a - b) < 0.001;
 }
